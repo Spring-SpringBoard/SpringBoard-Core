d7210116bbfb55858e83c12138f38870e567837a
Gajo Petrovic <gajop@uns.ac.rs>
2013-01-03 18:07:13 +0100

    tested currently existing mods and added their results in modinfo.lua

--- modinfo.lua
+++ modinfo.lua
@@ -14,16 +14,33 @@ local modinfo = {
 	depend = {
 		"Spring Cursors",
         "Spring Features 1.0",
+
+        -- mods here :
 		-- enter your mod here!
---        "XTA 9.715 beta",
-        --"xta:latest",
-        --"s44v153_marketgarden.sdz"
---        "Balanced Annihilation V7.72",
---        "BAR.sdd",
-        "BAR-svn.sdd",
-  --     "ba763.sdd",
---        "es0.536.sdz",
-        --"Evolution RTS - test-209"
+--        "XTA 9.715 beta", --works        
+--        "Balanced Annihilation V7.72", -- works
+--        "Balanced Annihilation Reloaded test-887", -- works
+--        "BattleTech: Legacy 0.1.r471", --works
+--        "BrickCraft epenis16 11Jan2012", --works
+--        "Conflict Terra ver1.02", -- works with slight bugs
+--        "Contact beta 002", --buggy 
+--        "Eternal Annihilation V1.04", --works
+--        "Evolution RTS - test-209" -- errors (tobool, etc.)        
+--        "Frontier test-50", -- errors, is this older or newer than conflict terra?
+--        "Kernel Panic 4.4", -- how to run?
+--        "Micro Modules 1.09", --works i guess
+--        "Micronwars v0.037", -- errors, tobool, etc
+--        "NOTA 1.71",a -- works
+--        "Operation X v2.1", --segfaults spring??
+--        "Progressive Annihilation 1.5", -- works
+--        "Spring: 1944 v1.62 Operation Most III", --works
+--        "Spring Tanks RC5.2", -- buggy, units aren't attacking, doesn't load map (similar to kernel panic)
+--        "Static Friction 0.5 Stable", --works
+--        "The Cursed 1.242", -- errors (tobool, etc.)
+--        "THIS KRE test-83", -- works, may have some visual bugs
+--        "Void 3.0", -- works
+--        "Zero-K DOTA v1.1.1.1", -- broken like Zero-K
+--        "Zero-K v1.1.1.1", -- errors, tobool, etc.
 	}
 }
 
