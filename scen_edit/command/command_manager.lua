682213e6f770bb845139b9c2cb1ae6f467b84bb7
Gajo Petrovic <gajop@uns.ac.rs>
2013-02-02 02:20:11 +0100

    updated chili; created multiple-command mode (merges all commands into one for undo/redo purposes)

--- scen_edit/command/command_manager.lua
+++ scen_edit/command/command_manager.lua
@@ -10,38 +10,74 @@ function CommandManager:init(maxUndoSize, maxRedoSize)
     SCEN_EDIT.Include(SCEN_EDIT_COMMAND_DIR .. 'abstract_command.lua')
     SCEN_EDIT.Include(SCEN_EDIT_COMMAND_DIR .. 'undoable_command.lua')
     SCEN_EDIT.IncludeDir(SCEN_EDIT_COMMAND_DIR)
+    --TODO: implement player lock
+    self.playerLock = nil --if set, it defines the id of the only player who can do commands
+    self.multipleCommandStack = {}
+    self.multipleCommandMode = false
 end
 
+--entering this mode will add all future commands executed in the .multipleCommandStack (no command will go to the undoList)
+--leaving this mode will group all the executed commands in one CompoundCommand and put it on the undoList
+--undo/redo is disabled during this mode
+function CommandManager:enterMultipleCommandMode()
+    assert(not self.multipleCommandMode, "Trying to enter multiple command mode while already in it")
+    self.multipleCommandMode = true
+end
+
+function CommandManager:leaveMultipleCommandMode()
+    assert(self.multipleCommandMode, "Trying to leave multiple command mode while not in it")
+    cmd = CompoundCommand(self.multipleCommandStack)
+    self.multipleCommandStack = {}
+    self:undoListAdd(cmd)
+    self.multipleCommandMode = false
+end
+
+--widget specifies whether the command should be executed in LuaUI(true) or LuaRules(false)
+--if the command is to be executed in a different lua state than currently in, it will send the message to the proper state using the message mechanism
 function CommandManager:execute(cmd, widget)
     assert(cmd, "Command is nil")
     if self.widget then
+    table.insert(self.redoList, cmd)
+    if #self.redoList > self.maxRedoSize then
+        table.remove(self.redoList, 1)
+    end
         if not widget then
             assert(cmd.className, "Command instance lacks className value")
             local msg = Message("command", cmd)
---            Spring.Echo('msg')
             SCEN_EDIT.messageManager:sendMessage(msg)
---            Spring.Echo('send')
         else
             cmd:execute()
         end
     else
         if not widget then
             cmd:execute()
-            if not cmd.unexecute then
---                Spring.Echo("not undoable")
-                return
+            if cmd.unexecute then
+                if self.multipleCommandMode then
+                    table.insert(self.multipleCommandStack, cmd)
+                else
+                    self:undoListAdd(cmd)
+                end
             end
---            Spring.Echo("undoable")
-            table.insert(self.undoList, cmd)
-            if #self.undoList > self.maxUndoSize then
-                table.remove(self.undoList, 1)
-            end
-            self.redoList = {}
         else
             assert(cmd.className, "Command instance lacks className value")
             local msg = Message("command", cmd)
             SCEN_EDIT.messageManager:sendMessage(msg)
         end
+    end
+end
+
+function CommandManager:undoListAdd(cmd)
+    table.insert(self.undoList, cmd)
+    if #self.undoList > self.maxUndoSize then
+        table.remove(self.undoList, 1)
+    end
+    self.redoList = {}
+end
+
+function CommandManager:redoListAdd(cmd)
+    table.insert(self.redoList, cmd)
+    if #self.redoList > self.maxRedoSize then
+        table.remove(self.redoList, 1)
     end
 end
 
@@ -51,16 +87,13 @@ function CommandManager:undo()
         SCEN_EDIT.messageManager:sendMessage(msg)
         return
     end
+    assert(not self.multipleCommandMode, "Cannot undo while in multiple command mode")
     if #self.undoList < 1 then
         return
     end
---    Spring.Echo("Undo")
     local cmd = table.remove(self.undoList, #self.undoList)
     cmd:unexecute()
-    table.insert(self.redoList, cmd)
-    if #self.redoList > self.maxRedoSize then
-        table.remove(self.redoList, 1)
-    end
+    self:redoListAdd(cmd)
 end
 
 function CommandManager:redo()
@@ -69,10 +102,10 @@ function CommandManager:redo()
         SCEN_EDIT.messageManager:sendMessage(msg)
         return
     end
+    assert(not self.multipleCommandMode, "Cannot redo while in multiple command mode")
     if #self.redoList < 1 then
         return
     end
---    Spring.Echo("Redo")
     local cmd = table.remove(self.redoList, #self.redoList)
     cmd:execute()
     table.insert(self.undoList, cmd)
