dd90734390d0c55c214c0be82224f1fadfdeae76
gajop <gajo01@gmail.com>
2013-07-22 18:56:03 +0200

    fix right click behaviour when changing terrain texture

--- scen_edit/view/terrain_editor.lua
+++ scen_edit/view/terrain_editor.lua
@@ -47,10 +47,10 @@ function TerrainEditorView:init()
         dir = SCEN_EDIT_IMG_DIR .. "brush_textures/",
         width = "100%",
         height = "100%",
+		multiSelect = false,
     }
     textureImages.OnSelectItem = {
         function(obj, itemIdx, selected)
-            Spring.Echo(itemIdx, selected)
             if selected and itemIdx > 0 then
                 local item = textureImages.items[itemIdx]
                 self.paintTexture = item
@@ -58,9 +58,12 @@ function TerrainEditorView:init()
                 if currentState:is_A(TerrainChangeTextureState) then
                     currentState.paintTexture = item
                 else
-                    SCEN_EDIT.stateManager:SetState(TerrainChangeTextureState(self.paintTexture))
+                    SCEN_EDIT.stateManager:SetState(TerrainChangeTextureState(self.paintTexture, textureImages))
                 end
             end
+			if not selected then
+				SCEN_EDIT.stateManager:SetState(DefaultState())
+			end
         end
     }
 
