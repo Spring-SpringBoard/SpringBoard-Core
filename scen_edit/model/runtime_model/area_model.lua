f966ad5012d283420d74906c8f038b66be722319
Gajo Petrovic <gajop@uns.ac.rs>
2014-05-16 14:16:31 +0200

    fixed area models not being properly updated at runtime

--- scen_edit/model/runtime_model/area_model.lua
+++ scen_edit/model/runtime_model/area_model.lua
@@ -1,14 +1,14 @@
 AreaModel = LCS.class{}
 
-function AreaModel:init(id, area)
+function AreaModel:init(id)
     self.id = id
-    self.area = area
     self.unitsInArea = {}    
 end
 
 function AreaModel:UnitInArea(unitId)
     local x, y, z = Spring.GetUnitPosition(unitId)
-    local res = x >= self.area[1] and x < self.area[3] and z >= self.area[2] and z < self.area[4]    
+    local area = SCEN_EDIT.model.areaManager:getArea(self.id)
+    local res = x >= area[1] and x < area[3] and z >= area[2] and z < area[4]    
     return res
 end
 
@@ -19,15 +19,11 @@ function AreaModel:UpdateUnit(unitId)
             return 'none'
         else
             self.unitsInArea[unitId] = true
-            local area = self.area
-            --Spring.Echo("E", unitId, self.id, area[1], area[2], area[3], area[4])
             return 'enter'
         end        
     else
         if self.unitsInArea[unitId] then
             self.unitsInArea[unitId] = nil
-            local area = self.area
-            --Spring.Echo("L", unitId, self.id, area[1], area[2], area[3], area[4])
             return 'leave'
         else
             return 'none'
