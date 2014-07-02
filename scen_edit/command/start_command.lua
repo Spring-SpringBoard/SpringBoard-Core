3b830ddb05c47365fa58555677b544c78ec75d50
Gajo Petrovic <gajop@uns.ac.rs>
2014-07-02 11:59:22 +0200

    now restores the heightmap to what it was before running the scenario

--- scen_edit/command/start_command.lua
+++ scen_edit/command/start_command.lua
@@ -8,6 +8,11 @@ function StartCommand:execute()
     if not SCEN_EDIT.rtModel.hasStarted then
         local oldModel = SCEN_EDIT.model:Serialize()
         SCEN_EDIT.model.oldModel = oldModel
+
+        local heightMap = HeightMap()
+        heightMap:Serialize()
+        SCEN_EDIT.model.oldHeightMap = heightMap
+
         SCEN_EDIT.rtModel:LoadMission(SCEN_EDIT.model:GetMetaData())
         local allUnits = Spring.GetAllUnits()
         for i = 1, #allUnits do
