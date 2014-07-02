ff77a7b4fc97f74c7a6f5713bb384f2f035b2e6f
Gajo Petrovic <gajop@uns.ac.rs>
2014-07-02 16:07:32 +0200

    cleaned up util.lua a bit and added tooltip in the Expression button (really needs a refactor..)

--- scen_edit/view/panels/abstract_type_panel.lua
+++ scen_edit/view/panels/abstract_type_panel.lua
@@ -46,7 +46,7 @@ end
 
 function AbstractTypePanel:MakeVariableOpt()
     --VARIABLE
-    self.cbVariable, self.cmbVariable = MakeVariableChoice(self.dataType, self.parent)
+    self.cbVariable, self.cmbVariable = self:MakeVariableChoice(self.dataType, self.parent)
     if self.cbVariable then
         table.insert(self.radioGroup, self.cbVariable)
     end
@@ -54,7 +54,7 @@ end
 
 function AbstractTypePanel:MakeExpressionOpt()
     --EXPRESSION
-    self.cbExpression, self.btnExpression = SCEN_EDIT.AddExpression(self.dataType, self.parent)
+    self.cbExpression, self.btnExpression = self:AddExpression(self.dataType, self.parent)
     if self.cbExpression then
         table.insert(self.radioGroup, self.cbExpression)
     end
@@ -93,7 +93,88 @@ function AbstractTypePanel:UpdatePanel(field)
             self.cbExpression:Toggle()
         end
         self.btnExpression.data = field.expr
+        local tooltip = SCEN_EDIT.humanExpression(self.btnExpression.data[1], "condition")
+        self.btnExpression.tooltip = tooltip
         return true
     end
     return false
 end
+
+function AbstractTypePanel:AddExpression(dataType, parent)
+    local viableExpressions = SCEN_EDIT.metaModel.functionTypesByOutput[dataType]
+    if viableExpressions then
+        local stackPanel = MakeComponentPanel(parent)
+        local cbExpressions = Checkbox:New {
+            caption = "Expression: ",
+            right = 100 + 10,
+            x = 1,
+            checked = false,
+            parent = stackPanel,
+        }
+        local btnExpressions = Button:New {
+            caption = 'Expression',
+            right = 1,
+            width = 100,
+            height = SCEN_EDIT.conf.B_HEIGHT,
+            parent = stackPanel,
+            data = {},
+        }
+        btnExpressions.OnClick = {
+            function()
+                local mode = 'add'
+                if #btnExpressions.data > 0 then
+                    mode = 'edit'
+                end
+                CustomWindow(parent.parent.parent, mode, dataType, btnExpressions.data, btnExpressions.data[1], cbExpressions, btnExpressions)
+            end
+        }
+        return cbExpressions, btnExpressions
+    end    
+    return nil, nil
+end
+
+function AbstractTypePanel:MakeVariableChoice(variableType, panel)
+    local variablesOfType = SCEN_EDIT.model.variableManager:getVariablesOfType(variableType)
+    if not variablesOfType then
+        return nil, nil
+    end
+    local variableNames = {}
+    local variableIds = {}
+    for id, variable in pairs(variablesOfType) do
+        table.insert(variableNames, variable.name)
+        table.insert(variableIds, id)
+    end
+
+    if #variableIds > 0 then
+        local stackPanel = MakeComponentPanel(panel)
+        local cbVariable = Checkbox:New {
+            caption = "Variable: ",
+            right = 100 + 10,
+            x = 1,
+            checked = false,
+            parent = stackPanel,
+        }
+        
+        local cmbVariable = ComboBox:New {
+            right = 1,        
+            width = 100,
+            height = SCEN_EDIT.conf.B_HEIGHT,
+            parent = stackPanel,
+            items = variableNames,
+            variableIds = variableIds,
+        }
+        cmbVariable.OnSelect = {
+            function(obj, itemIdx, selected)
+                if selected and itemIdx > 0 then
+                    if not cbVariable.checked then
+                        cbVariable:Toggle()
+                    end
+                end
+            end
+        }
+        return cbVariable, cmbVariable
+    else
+        return nil, nil
+    end
+end
+
