cd2709371a8c198fe511f9b5aea29273a4fdbc54
Gajo Petrovic <gajop@uns.ac.rs>
2014-07-03 20:10:29 +0200

    fixed a few bugs with lacking unit model ID, removed trigger unit type special

--- scen_edit/view/panels/unit_type_panel.lua
+++ scen_edit/view/panels/unit_type_panel.lua
@@ -40,43 +40,10 @@ function UnitTypePanel:MakePredefinedOpt()
     }
 end
 
-function UnitTypePanel:MakeSpecialOpt()
-    --SPECIAL TYPE, i.e TRIGGER
-    local stackUnitTypePanel = MakeComponentPanel(self.parent)
-    self.cbSpecialType = Checkbox:New {
-        caption = "Special type: ",
-        right = 100 + 10,
-        x = 1,
-        checked = true,
-        parent = stackUnitTypePanel,
-    }
-    table.insert(self.radioGroup, self.cbSpecialType)
-    self.cmbSpecialType = ComboBox:New {
-        right = 1,
-        width = 100,
-        height = SCEN_EDIT.conf.B_HEIGHT,
-        parent = stackUnitTypePanel,
-        items = { "Trigger unit type" },
-        OnSelect = {
-            function(obj, itemIdx, selected)
-                if selected and itemIdx > 0 then
-                    if not self.cbSpecialType.checked then
-                        self.cbSpecialType:Toggle()
-                    end
-                end
-            end
-        },
-    }
-end
-
 function UnitTypePanel:UpdateModel(field)
     if self.cbPredefinedType and self.cbPredefinedType.checked and self.btnPredefinedType.unitTypeId ~= nil then
         field.type = "pred"
         field.id = self.btnPredefinedType.unitTypeId
-        return true
-    elseif self.cbSpecialType and self.cbSpecialType.checked then
-        field.type = "spec"
-        field.name = self.cmbSpecialType.items[self.cmbSpecialType.selected]
         return true
     end
     return self:super('UpdateModel', field)
@@ -88,12 +55,6 @@ function UnitTypePanel:UpdatePanel(field)
             self.cbPredefinedType:Toggle()
         end
         self.btnPredefinedType.OnSelectUnitType[1](field.id)
-        return true
-    elseif field.type == "spec" then
-        if not self.cbSpecialType.checked then
-            self.cbSpecialType:Toggle()
-        end
-        self.cmbSpecialType:Select(1) --TODO:fix it        
         return true
     end
     return self:super('UpdatePanel', field)
