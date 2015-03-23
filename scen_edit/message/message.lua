de45976024b3617ac02e3a8d3985227110c2e44a
Gajo Petrovic <gajo01@gmail.com>
2015-03-24 06:16:13 +0900

    fix LCS issues with serialization (again using master)

--- scen_edit/message/message.lua
+++ scen_edit/message/message.lua
@@ -6,8 +6,22 @@ function Message:init(tag, data)
 end
 
 function Message:serialize()
-    return {
+    return self:deepcopyLCS({
         tag = self.tag,
         data = self.data,
-    }
+    })
+end
+
+function Message:deepcopyLCS(t)
+    if type(t) ~= 'table' then return t end
+    local res = {}
+    for k, v in pairs(t) do
+        if type(v) ~= "function" and k ~= "__index" then
+            if type(v) == 'table' then
+                v = self:deepcopyLCS(v)
+            end
+            res[k] = v
+        end
+    end
+    return res
 end
