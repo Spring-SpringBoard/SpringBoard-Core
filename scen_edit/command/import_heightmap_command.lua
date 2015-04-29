09d5cd234821f97488a1eb7f6f246ab3ca74f064
Gajo Petrovic <gajo01@gmail.com>
2015-04-29 23:39:10 +0900

    close #45

---
+++ scen_edit/command/import_heightmap_command.lua
@@ -0,0 +1,69 @@
+ImportHeightmapCommand = AbstractCommand:extends{}
+ImportHeightmapCommand.className = "ImportHeightmapCommand"
+
+function ImportHeightmapCommand:init(heightmapImage, maxHeight, minHeight)
+    self.className = "ImportHeightmapCommand"
+    self.heightmapImagePath = heightmapImage
+    self.maxHeight = maxHeight
+    self.minHeight = minHeight
+end
+
+function ImportHeightmapCommand:execute()
+    SCEN_EDIT.delayGL(function()
+        if not VFS.FileExists(self.heightmapImagePath) then
+            Spring.Echo("Missing heightmap file: " .. tostring(self.heightmapImagePath))
+            return
+        end
+
+        Spring.Log("scened", LOG.DEBUG, "Importing heightmap..")
+        local heightmapTexture = gl.CreateTexture(Game.mapSizeX / Game.squareSize + 1, Game.mapSizeZ / Game.squareSize + 1, {
+            border = false,
+            min_filter = GL.NEAREST,
+            mag_filter = GL.NEAREST,
+            wrap_s = GL.CLAMP_TO_EDGE,
+            wrap_t = GL.CLAMP_TO_EDGE,
+            fbo = true,
+        })
+
+        local texInfo = gl.TextureInfo(heightmapTexture)
+        local w, h = texInfo.xsize, texInfo.ysize
+        local res
+
+        gl.RenderToTexture(heightmapTexture, function()
+            gl.Texture(self.heightmapImagePath)
+            gl.TexRect(-1,-1, 1, 1, 0, 0, 1, 1)
+            res = gl.ReadPixels(0, 0, w, h)
+            gl.DeleteTexture(self.heightmapImagePath)
+        end)
+
+        local greyscale = {}
+        for i, row in pairs(res) do
+            greyscale[i] = {}
+            for j, point in pairs(row) do
+                greyscale[i][j] = (point[1] + point[2] + point[3]) / 3 * point[4]
+                greyscale[i][j] = self.minHeight + greyscale[i][j] * (self.maxHeight - self.minHeight)
+            end
+        end
+
+        SCEN_EDIT.commandManager:execute(ImportHeightmapCommandSynced(greyscale))
+    end)
+end
+
+ImportHeightmapCommandSynced = AbstractCommand:extends{}
+ImportHeightmapCommandSynced.className = "ImportHeightmapCommandSynced"
+
+function ImportHeightmapCommandSynced:init(greyscale)
+    self.className = "ImportHeightmapCommandSynced"
+    self.greyscale = greyscale
+end
+
+function ImportHeightmapCommandSynced:execute()
+    Spring.SetHeightMapFunc(function()
+        for z = 0, Game.mapSizeZ, Game.squareSize do
+            for x = 0, Game.mapSizeX, Game.squareSize do
+                local column = self.greyscale[z / Game.squareSize + 1]
+                Spring.SetHeightMap(x, z, column[x / Game.squareSize + 1])
+            end
+        end
+    end)
+end
--- scen_edit/view/actions/import_action.lua
+++ scen_edit/view/actions/import_action.lua
@@ -17,6 +17,54 @@ function ImportAction:execute()
                 local importCommand = ImportDiffuseCommand(path)
                 SCEN_EDIT.commandManager:execute(importCommand, true)
                 Spring.Log("scened", LOG.NOTICE, "Import complete.")
+            elseif fileType == fileTypes[3] then
+                local ebMinHeight = EditBox:New {
+                    hint = "Min height: ",
+                    text = ""
+                }
+                local ebMaxHeight = EditBox:New {
+                    hint = "Max height: ",
+                    text = "",
+                }
+                local window
+                window = Window:New {
+                    width = 200,
+                    height = 200,
+                    x = 550,
+                    y = 350,
+                    parent = screen0,
+                    children = {
+                        StackPanel:New {
+                            x = 0, y = 0,
+                            right = 0, bottom = 0,
+                            children = {
+                                ebMinHeight,
+                                ebMaxHeight,
+                                Button:New {
+                                    caption = "OK",
+                                    OnClick = { function()
+                                        local maxHeight = tonumber(ebMaxHeight.text)
+                                        local minHeight = tonumber(ebMinHeight.text)
+                                        if maxHeight == nil or minHeight == nil then
+                                            return
+                                        end
+                                        Spring.Log("scened", LOG.NOTICE, "Importing heightmap: " .. path .. " ...")
+                                        local importCommand = ImportHeightmapCommand(path, maxHeight, minHeight)
+                                        SCEN_EDIT.commandManager:execute(importCommand, true)
+                                        Spring.Log("scened", LOG.NOTICE, "Import complete.")
+                                        window:Dispose()
+                                    end},
+                                },
+                                Button:New {
+                                    caption = "Cancel",
+                                    OnClick = { function()
+                                        window:Dispose()
+                                    end},
+                                },
+                            },
+                        }
+                    },
+                }
             else
                 Spring.Log("scened", LOG.ERROR, "Error trying to export. Invalida fileType specified: " .. tostring(fileType))
             end
