4e278a0d6059d04a5d2370c078198b714eb0d970
Gajo Petrovic <gajo01@gmail.com>
2017-08-03 09:24:41 +0900

    optimize feature drawing for memory

--- scen_edit/view/selection_manager.lua
+++ scen_edit/view/selection_manager.lua
@@ -4,6 +4,8 @@ local AreaListener = AreaManagerListener:extends{}
 function SelectionManager:init()
     self:super("init")
     self.selectedUnits = {}
+    -- self.selectedFeatures is a table of: feature ID -> true
+    -- this is done to have faster updates
     self.selectedFeatures = {}
     self.selectedAreas = {}
     self.areaListener = AreaListener(self)
@@ -11,9 +13,13 @@ function SelectionManager:init()
 end
 
 function SelectionManager:GetSelection()
+    local features = {}
+    for featureID, _ in pairs(self.selectedFeatures) do
+        table.insert(features, featureID)
+    end
     local selection = {
         units = self.selectedUnits,
-        features = self.selectedFeatures,
+        features = features,
         areas = self.selectedAreas,
     }
     return selection
@@ -41,7 +47,12 @@ function SelectionManager:Select(selection)
 
     self.selectedUnits = selection.units or {}
     Spring.SelectUnitArray(self.selectedUnits)
-    self.selectedFeatures = selection.features or {}
+
+    local features = selection.features or {}
+    for _, featureID in pairs(features) do
+        self.selectedFeatures[featureID] = true
+    end
+
     self.selectedAreas = selection.areas or {}
     for _, areaID in pairs(self.selectedAreas) do
         SB.view.areaViews[areaID].selected = true
@@ -60,15 +71,12 @@ function SelectionManager:Update()
     self.selectedUnits = selUnits
     Spring.SelectUnitArray(self.selectedUnits)
 
-    local newSelectedFeatures = {}
-    for _, featureID in pairs(self.selectedFeatures) do
-        if Spring.ValidFeatureID(featureID) then
-            table.insert(newSelectedFeatures, featureID)
-        else
+    for featureID, _ in pairs(self.selectedFeatures) do
+        if not Spring.ValidFeatureID(featureID) then
+            self.selectedFeatures[featureID] = nil
             changed = true
         end
     end
-    self.selectedFeatures = newSelectedFeatures
     --[[
     if #unitIDs ~= #self.selectedUnits then
         self:__ClearSelection()
@@ -82,30 +90,46 @@ function SelectionManager:Update()
     end
 end
 
+-- we cache minx, maxx, minz, maxz for each feature def
+-- this saves us a lot of memory
+local __cachedDefs = {}
+local function _GetFeatureDefSize(featureDefID)
+    if __cachedDefs[featureDefID] == nil then
+        local featureDef = FeatureDefs[featureDefID]
+        local minx, maxx = featureDef.model.minx or -10, featureDef.model.maxx or 10
+        local minz, maxz = featureDef.model.minz or -10, featureDef.model.maxz or 10
+        if maxx - minx < 20 then
+            minx, maxx = -10, 10
+        end
+        if maxz - minz < 20 then
+            minz, maxz = -10, 10
+        end
+        __cachedDefs[featureDefID] = {minx, maxx, minz, maxz}
+    end
+    local c = __cachedDefs[featureDefID]
+    return c[1], c[2], c[3], c[4]
+end
+
+local function DrawLines(x1, x2, z1, z2, by)
+    gl.Vertex(x1, by, z1)
+    gl.Vertex(x2, by, z1)
+    gl.Vertex(x2, by, z2)
+    gl.Vertex(x1, by, z2)
+    gl.Vertex(x1, by, z1)
+end
+
 function SelectionManager:DrawWorldPreUnit()
-    local selection = self:GetSelection()
-    for _, featureID in pairs(selection.features) do
+    if false then
+        return
+    end
+    gl.Color(0, 1, 0, 1)
+    for featureID, _ in pairs(self.selectedFeatures) do
         if Spring.ValidFeatureID(featureID) then
             local bx, by, bz = Spring.GetFeaturePosition(featureID)
-            local featureDef = FeatureDefs[Spring.GetFeatureDefID(featureID)]
-            local minx, maxx = featureDef.model.minx or -10, featureDef.model.maxx or 10
-            local minz, maxz = featureDef.model.minz or -10, featureDef.model.maxz or 10
-            if maxx - minx < 20 then
-                minx, maxx = -10, 10
-            end
-            if maxz - minz < 20 then
-                minz, maxz = -10, 10
-            end
+            local minx, maxx, minz, maxz = _GetFeatureDefSize(Spring.GetFeatureDefID(featureID))
             local x1, z1 = bx + minx - 5, bz + minz + 5
             local x2, z2 = bx + maxx - 5, bz + maxz + 5
-            gl.BeginEnd(GL.LINE_STRIP, function()
-                gl.Color(0, 1, 0, 1)
-                gl.Vertex(x1, by, z1)
-                gl.Vertex(x2, by, z1)
-                gl.Vertex(x2, by, z2)
-                gl.Vertex(x1, by, z2)
-                gl.Vertex(x1, by, z1)
-            end)
+            gl.BeginEnd(GL.LINE_STRIP, DrawLines, x1, x2, z1, z2, by)
         end
     end
 end
