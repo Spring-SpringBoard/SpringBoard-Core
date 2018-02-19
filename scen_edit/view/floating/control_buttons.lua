63362da4931509853656aa020cab89c1a49d7d15
Gajo Petrovic <gajo01@gmail.com>
2018-02-20 02:06:04 +0900

    fix control button start/stop

--- scen_edit/view/floating/control_buttons.lua
+++ scen_edit/view/floating/control_buttons.lua
@@ -165,10 +165,10 @@ function ControlButtons:GameStarted()
     self.btnStartStop.backgroundColor = SB.conf.BTN_CANCEL_COLOR
     self.btnStartStop.Update = function(obj, ...)
         Chili.Button.Update(obj, ...)
-        self.backgroundColor = SB.deepcopy(SB.conf.BTN_CANCEL_COLOR)
-        self.backgroundColor[4] = 0.5 + math.abs(2 * math.sin(os.clock())) / math.pi
-        self:Invalidate()
-        self:RequestUpdate()
+        obj.backgroundColor = SB.deepcopy(SB.conf.BTN_CANCEL_COLOR)
+        obj.backgroundColor[4] = 0.5 + math.abs(2 * math.sin(os.clock())) / math.pi
+        obj:Invalidate()
+        obj:RequestUpdate()
     end
 
     self:UpdateGameDrawing()
