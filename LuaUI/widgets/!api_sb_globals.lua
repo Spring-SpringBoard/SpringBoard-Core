3bd938786c8243d8920208accfc00925d586c7ac
Gajo Petrovic <gajo01@gmail.com>
2018-12-08 13:50:04 +0900

    suppress error

--- LuaUI/widgets/!api_sb_globals.lua
+++ LuaUI/widgets/!api_sb_globals.lua
@@ -1,2 +1,15 @@
+function widget:GetInfo()
+    return {
+        name      = "sb_globals",
+        author    = "gajop",
+        date      = "in the future",
+        license   = "GPL-v2",
+        layer     = -9999999999,
+        enabled   = true,
+        api       = true,
+        hidden    = true,
+    }
+end
+
 WG.SB_LIBS_DIR = "libs_sb/"
 WG.SB = {}
