30f1a23568d76dbe6845853c84149e9019878c9b
Gajo Petrovic <gajo01@gmail.com>
2018-12-18 20:25:50 +0900

    add sounds of silence to silence the errors of sounds.lua

---
+++ Gamedata/sounds.lua
@@ -0,0 +1,18 @@
+-- TODO: Spring shouldn't default sounds
+
+return {
+	SoundItems = {
+		IncomingChat = {
+			file = "sounds/silence.wav",
+		},
+		MultiSelect = {
+			file = "sounds/silence.wav",
+		},
+		MapPoint = {
+			file = "sounds/silence.wav",
+		},
+		FailedCommand = {
+			file = "sounds/silence.wav",
+		},
+	},
+}
