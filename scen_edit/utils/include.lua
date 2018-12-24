e0bd4281b37da1631e71c32a83d5fc72507c69ff
Gajo Petrovic <gajo01@gmail.com>
2018-12-24 11:23:36 +0900

    move savetable to libs folder

--- scen_edit/utils/include.lua
+++ scen_edit/utils/include.lua
@@ -1,5 +1,5 @@
 -- Shared include (widget & gadget)
-VFS.Include("savetable.lua")
+SB.Include(LIBS_DIR .. "savetable.lua")
 -- SB.Include(LIBS_DIR .. "utils/luaunit.lua")
 SB.IncludeDir(LIBS_DIR .. "utils/")
 Log.SetLogSection("SpringBoard")
